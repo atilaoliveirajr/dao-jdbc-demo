dd12749f8fb208285f8d6d1d2890b7b9ade1a4fa
Átila <atila.oliveira.jr@gmail.com>
2020-06-04 09:38:52 +0200

    Reusing instantiation

--- src/application/Programm.java
+++ src/application/Programm.java
@@ -4,18 +4,18 @@ import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
 
-
 public class Programm {
-	
+
 	public static void main(String[] args) {
-		
-			
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		System.out.println("===TESTE 1: seller findById === ");
 		
 		Seller seller = sellerDao.findById(3);
-		
+
 		System.out.println(seller);
-		
+
 	}
 
 }
