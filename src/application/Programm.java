185960bfc95504c326e1e5ef0f93b7f20c21de77
Átila <atila.oliveira.jr@gmail.com>
2020-06-06 09:55:21 +0200

    update implemented

--- src/application/Programm.java
+++ src/application/Programm.java
@@ -32,10 +32,16 @@ public class Programm {
 			System.out.println(obj);
 		}
 
-		System.out.println("\n===TEST 4: seller insert === ");
-		Seller newSeller = new Seller(null, "Dave", "dave@gmail.com", new Date(), 3000.0, department2);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		System.out.println("\n===TEST 4: seller insert === ");
+//		Seller newSeller = new Seller(null, "Dave", "dave@gmail.com", new Date(), 3000.0, department2);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n===TEST 5: seller update === ");
+		seller = sellerDao.findById(1);
+		seller.setName("Bob Lila");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
